cfa083c1e16bb1511ed7cc84d8a3a0ea8bd20ca6
MicahJank <MicahJank@users.noreply.github.com>
2020-02-19 21:27:06 -0600

    filled out axiosWithAuth component

---
+++ src/utils/AxiosWithAuth.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+
+const axiosWithAuth = () => {
+    const token = localStorage.getItem('token');
+
+    return axios.create({
+        baseURL: '',
+        headers: {
+            Authorization: token
+        }
+    })
+};
+
+export default axiosWithAuth;
