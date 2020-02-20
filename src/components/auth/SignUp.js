fa397e2fd5481d6fe2f2514cc54e79e322db43d0
Katrina Roaix <kroaix@gmail.com>
2020-02-19 23:56:49 -0500

    basic form and Google sign in button

---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Button, Icon, Header, Divider, Form, Container } from "semantic-ui-react";
+
+const SignUp = () => {
+  return (
+    <Container text>
+      <div align="center">
+        <Header as="h2" icon>
+          <Icon name="home" />
+          Welcome to TidyHive!
+          <Header.Subheader>Sign up to get started.</Header.Subheader>
+        </Header>
+      </div>
+      <div>
+        <Form>
+          <Form.Field>
+            <label>Username</label>
+            <input placeholder="Username" />
+          </Form.Field>
+          <Form.Field>
+            <label>Email</label>
+            <input placeholder="Email" />
+          </Form.Field>
+          <Form.Field>
+            <label>Password</label>
+            <input placeholder="Password" />
+          </Form.Field>
+          <Form.Field>
+            <label>Repeat Password</label>
+            <input placeholder="Repeat Password" />
+          </Form.Field>
+          <Button type="submit">Submit</Button>
+        </Form>
+      </div>
+      <Divider horizontal>OR</Divider>
+      <div align="center">
+        <Button icon>
+          <Icon name="google" />
+          &nbsp;&nbsp;&nbsp;Sign up with Google
+        </Button>
+      </div>
+    </Container>
+  );
+};
+
+export default SignUp;
