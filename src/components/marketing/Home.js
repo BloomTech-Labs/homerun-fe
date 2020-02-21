071d9ace66dd4e2ec7d1b8b152c44c99a6c1a64e
MicahJank <MicahJank@users.noreply.github.com>
2020-02-20 23:16:02 -0600

    added a button that takes the user to the email sign up component

--- src/components/marketing/Home.js
+++ src/components/marketing/Home.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
+import { NavLink } from 'react-router-dom';
 
+import { Button } from 'semantic-ui-react';
+
+// right now the button takes us to the email sign up but from the Home page the next step would actually be the initial sign in page where the user can choose to sign in with
+// google or email - once those components are built out we can have the navlink take us to that route
 const Home = () => {
 
     return (
+        <>
         <div>
             Home
         </div>
+
+        <Button>
+            <NavLink to="/email/signup">Go to Email Sign Up</NavLink>
+        </Button>
+        </>
     );
 };
 
