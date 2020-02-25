ca770179d97e065668c96fb2bba0d83ed4544601
Katrina Roaix <kroaix@gmail.com>
2020-02-24 23:51:35 -0500

    Added some routes and imported sign in components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+// Sign up component with form for email and gmail button
 import SignUp from './components/auth/SignUp';
+
+//Sign in landing page with two buttons email/gmail
+import SignInLanding from './components/auth/SignIn-Landing';
+
+//Component for email sign in
+import SignInEmail from './components/auth/SignIn-Email';
 
 // Home component is where the beginning of the app starts
 import Home from './components/marketing/Home.js';
@@ -11,10 +18,11 @@ import "semantic-ui-css/semantic.min.css";
 function App() {
   return (
     <div className="App">
-
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/email/signup" component={SignUp} /> 
+        <Route exact path="/signin" component={SignInLanding} />
+        <Route path="/signin/email" component={SignInEmail} />
+        <Route path="/signup" component={SignUp} /> 
       </Switch>
     </div>
   );
