ea5600397a103cb54f717fc31a97a8d301d9e1d0
Katrina Roaix <kroaix@gmail.com>
2020-02-24 23:50:41 -0500

    SignIn Landing page with two buttons

--- src/components/auth/SignIn-Landing.js
+++ src/components/auth/SignIn-Landing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 
 const SignInLanding = () => {
   return (
@@ -8,8 +8,14 @@ const SignInLanding = () => {
         <Icon name="google" />
         &nbsp;&nbsp;&nbsp;Sign in with Google
       </Button>
+      <br />
+      <br />
       <Button>Sign in with Email</Button>
-      <p>Don't have an account? <a href="">Sign Up</a></p>
+      <br />
+      <br />
+      <p>
+        Don't have an account? <a href="/signin/email">Sign Up</a>
+      </p>
     </div>
   );
 }
