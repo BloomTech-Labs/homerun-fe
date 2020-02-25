7f8710e377d2e29215f0f97415ab2a2a6ad60808
Mayankho <yankhot96@gmail.com>
2020-02-24 20:39:48 -0500

    Imported analytics and invoked in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import SignUp from './components/auth/SignUp';
 import "semantic-ui-css/semantic.min.css";
+import ReactGA from "react-ga";
+
+
+
+function initializeAnalytics (){
+
+  ReactGA.initialize("UA-150184896-3");
+  ReactGA.pageview('/SignUp');
+}
+
+
 
 function App() {
+  initializeAnalytics()
   return (
     <div className="App">
       <SignUp />
