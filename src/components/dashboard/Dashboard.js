f6faccfb5b607d2254869c71e217aaf527024a43
MicahJank <MicahJank@users.noreply.github.com>
2020-02-25 20:54:57 -0600

    imported the TodoForm and have it displaying at the bottom

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import TodoForm from './TodoForm.js';
+
 
 const Dashboard = () => {
 
     return (
+        <>
         <h1>Dashboard</h1>
+
+        <TodoForm />
+        </>
     )
 }
 
