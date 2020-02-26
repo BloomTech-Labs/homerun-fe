0d2dc621dab6713e91ade506cbd7c1c521d39e9a
MicahJank <MicahJank@users.noreply.github.com>
2020-02-25 22:50:15 -0600

    updated the site.variables

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'semantic-ui-less/semantic.less';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
