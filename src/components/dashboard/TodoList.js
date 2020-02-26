ba3c20bda018ac58c0c39d381aa164ac7cfd8dde
Mayankho <yankhot96@gmail.com>
2020-02-25 22:36:58 -0500

    TodoList component

---
+++ src/components/dashboard/TodoList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSwipeable, Swipeable } from 'react-swipeable';
+
+
+const TodoList = ({todo, remove}) => {
+
+
+
+
+}
+
+
+
+export default TodoList;
+
+
+
+
