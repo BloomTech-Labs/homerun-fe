19090c22a2a3eeb75f240e90cd0f7cc7fe674da7
Katrina Roaix <kroaix@gmail.com>
2020-02-27 22:22:14 -0500

    login functionality implemented

--- src/components/auth/SignIn-Email.js
+++ src/components/auth/SignIn-Email.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Icon,
   Header,
   Form,
-  Container
+  Container,
+  Loader,
+  Dimmer
 } from "semantic-ui-react";
+import axios from "axios";
 
-const SignInEmail = () => {
+const SignInEmail = props => {
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: ""
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setIsLoading(true);
+    axios
+      .post("https://stage-homerun-be.herokuapp.com/auth/login", credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/dashboard");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const handleChange = e => {
+    e.preventDefault();
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
   return (
     <Container text>
       <div align="center">
@@ -18,17 +46,35 @@ const SignInEmail = () => {
         </Header>
       </div>
       <div>
-        <Form>
-          <Form.Field>
-            <label>Email</label>
-            <input type="email" placeholder="Email" />
-          </Form.Field>
-          <Form.Field>
-            <label>Password</label>
-            <input type="password" placeholder="Password" />
-          </Form.Field>
-          <Button type="submit">Submit</Button>
-        </Form>
+        {isLoading ? (
+          <Dimmer active inverted>
+            <Loader size="large">Loading</Loader>
+          </Dimmer>
+        ) : (
+          <Form onSubmit={handleSubmit}>
+            <Form.Field>
+              <label>Email</label>
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={credentials.email}
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={credentials.password}
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <Button type="submit">Submit</Button>
+          </Form>
+        )}
       </div>
     </Container>
   );
