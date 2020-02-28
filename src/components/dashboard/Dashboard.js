c80452c4b01b6ff15e1d4029ea6b6c5ad5afac9e
Katrina Roaix <kroaix@gmail.com>
2020-02-27 22:25:16 -0500

    Added logout button to test private routes

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Button } from 'semantic-ui-react';
+import TodoForm from "./TodoForm.js";
 
-import TodoForm from './TodoForm.js';
-
-
-const Dashboard = () => {
-
-    return (
-        <>
-        <h1>Dashboard</h1>
-
-        <TodoForm />
-        </>
-    )
-}
+const Dashboard = props => {
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <Button
+        onClick={() => {
+          localStorage.removeItem("token");
+          props.history.push("/signin");
+        }}
+      >
+        Logout
+      </Button>
+      <TodoForm />
+    </>
+  );
+};
 
 export default Dashboard;
-
