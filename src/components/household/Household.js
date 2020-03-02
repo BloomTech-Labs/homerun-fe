3003d4d8b393593eb3498027734ef5d24aa6e5e8
Micah <micahjank@gmail.com>
2020-03-02 15:56:09 -0600

    added 2 buttons to the household.js component, one for joining household and another for going to the dashboard

--- src/components/household/Household.js
+++ src/components/household/Household.js
@@ -2,12 +2,22 @@ import React from "react";
 import List from "./List";
 import HouseholdForm from "./HouseholdForm";
 
+import { Button, Icon } from 'semantic-ui-react';
+
 const Household = () => {
 
     return (
         <div>
+            <Button>Join Household?</Button>
             <List />
-
+            <Button animated>
+                <Button.Content visible>
+                    Dashboard
+                </Button.Content>
+                <Button.Content hidden>
+                    <Icon name='arrow right' />
+                </Button.Content>
+            </Button>
         </div>
     )
 }
