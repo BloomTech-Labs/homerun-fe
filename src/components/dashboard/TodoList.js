b554298d638ae464d710635a1b5abcb947165daf
Katrina Roaix <kroaix@gmail.com>
2020-03-02 23:45:07 -0500

    Added temporary icons

--- src/components/dashboard/TodoList.js
+++ src/components/dashboard/TodoList.js
@@ -1,27 +1,27 @@
 import React from 'react'
-import { Image, List } from 'semantic-ui-react'
+import { List, Icon } from 'semantic-ui-react'
 
-const TodoList= () => (
-  <List divided verticalAlign='middle'>
+const TodoList = () => (
+  <List divided verticalAlign="middle">
     <List.Item>
-      <Image avatar src='' />
+      <Icon name="check" />
       <List.Content>
-        <List.Header as='a'>Task 1 </List.Header>
+        <List.Header as="a">Task 1 </List.Header>
       </List.Content>
     </List.Item>
     <List.Item>
-      <Image avatar src='' />
+      <Icon name="check" />
       <List.Content>
-        <List.Header as='a'>Task 2</List.Header>
+        <List.Header as="a">Task 2</List.Header>
       </List.Content>
     </List.Item>
     <List.Item>
-      <Image avatar src='' />
+      <Icon name="check" />
       <List.Content>
-        <List.Header as='a'>Task 3</List.Header>
+        <List.Header as="a">Task 3</List.Header>
       </List.Content>
     </List.Item>
   </List>
-)
+);
 
 export default TodoList;
