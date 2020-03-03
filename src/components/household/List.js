21528b49788ab9331fb94f7339ca023b71f06976
MicahJank <MicahJank@users.noreply.github.com>
2020-03-02 22:45:25 -0600

    added some new names to the list in the list.js component

--- src/components/household/List.js
+++ src/components/household/List.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Name from "./Name";
 import { Button, Modal } from 'semantic-ui-react';
 import HouseholdForm from './HouseholdForm';
@@ -6,16 +6,18 @@ import HouseholdForm from './HouseholdForm';
 const List = () => {
  const [modalOpen, setModalOpen] = useState(false);
 
+
         return (
             <div>
-                <Name />
+                <Name name='Lilly' />
+                <Name name='Billy' />
+                <Name name='Silly' />
                 <Modal
                     open={modalOpen}
                     trigger={<Button onClick={() => setModalOpen(true)}>Add Member</Button>}
                     header='Add a member'
                     content={<HouseholdForm setModalOpen={setModalOpen} />}
                     >
-
                     </Modal>
             </div>
         )
