883a86ab4190ab6e86422be419ac4230c0fd2996
Micah <micahjank@gmail.com>
2020-03-04 16:37:08 -0600

    refactored the button for the modal into a function

--- src/components/header/PinNumber.js
+++ src/components/header/PinNumber.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-import { Button, Header, Modal } from 'semantic-ui-react';
+import { Button, Header, Modal, Icon } from 'semantic-ui-react';
+
+const ModalButton = () => {
+  return (
+    <button>
+      <Icon name='lock' size='big' />
+    </button>
+  )
+}
 
 const PinNumber = () => (
-    <Modal trigger={<Button className='circle-button' size='large' circular icon='lock'/>}>
+    <Modal trigger={ModalButton()}>
         <Modal.Header> Enter PIN </Modal.Header>
                 <label>Pin</label>
                 <input placeholder='Pin' />
