78a498f7aee0c2a3f557581fce5edd9fb6f03403
Micah <micahjank@gmail.com>
2020-03-04 16:36:34 -0600

    changed the component from using semantic ui button to using normal jsx because the stlying was getting thrown off

--- src/components/header/AccountSettings.js
+++ src/components/header/AccountSettings.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import { Button, Icon } from 'semantic-ui-react';
 
 const AccountSettings = () => {
 
     return (
-        <button>Settings</button>
+        <button>
+             <Icon size='big' name='user circle'/>
+        </button>
     )
 }
 
