1cb2e77e4e29892241c663dbab79ffc02d6cf291
Micah <micahjank@gmail.com>
2020-03-04 15:45:06 -0600

    added classes to the Button component and made it circular with an icon

--- src/components/header/PinNumber.js
+++ src/components/header/PinNumber.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Button, Header, Modal } from 'semantic-ui-react';
 
 const PinNumber = () => (
-    <Modal trigger={<Button> Pin Number </Button>}>
+    <Modal trigger={<Button className='pin-lock' size='large' circular icon='lock'/>}>
         <Modal.Header> Enter PIN </Modal.Header>
                 <label>Pin</label>
                 <input placeholder='Pin' />
