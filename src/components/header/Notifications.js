8488345142b33f398a5c9ca9b4f6555f5acda941
MicahJank <MicahJank@users.noreply.github.com>
2020-03-03 21:24:19 -0600

    created the notification button

---
+++ src/components/header/Notifications.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Button } from 'semantic-ui-react';
+
+const Notifications = () => {
+
+    return (
+        <Button circular icon='bell' />
+    )
+}
+
+export default Notifications;
