815c7f5cb0b49874373de5300818357cca1679f9
Katrina Roaix <kroaix@gmail.com>
2020-03-04 23:03:01 -0500

    added loader to confirm-account component

--- src/components/auth/Confirm-Account.js
+++ src/components/auth/Confirm-Account.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router';
 import axios from 'axios';
+import { Loader, Dimmer } from 'semantic-ui-react';
 
 const ConfirmAcct = props => {
+  const [isLoading, setIsLoading] = useState(true)
   let { hash } = useParams();
   axios
     .post("https://stage-homerun-be.herokuapp.com/auth/confirm", hash)
     .then(res => {
-      console.log(res);
+      setIsLoading(false)
       props.history.push("/signin/email");
+      console.log(res);
     })
     .catch(err => {
       console.log(err);
@@ -16,6 +19,11 @@ const ConfirmAcct = props => {
 
   return(
     <div>
+      {isLoading ? (
+        <Dimmer active inverted>
+          <Loader size="large">Loading</Loader>
+        </Dimmer> ) : (null)
+      }
     </div>
   )
 }
