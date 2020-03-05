898f1172ccdd6fd8d4cff5b05019089d77f84da5
Micah <micahjank@gmail.com>
2020-03-04 18:25:22 -0600

    updated header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,14 +6,20 @@ import PinNumber from './PinNumber.js';
 
 import '../../SASS/Header.scss';
 
+import { useLocation } from 'react-router-dom';
+
 import { Header as UiHeader } from 'semantic-ui-react';
 
 const Header = () => {
-    
+    // location is an object that contains the current url path on the 'pathname' property
+    const location = useLocation();
+
     return (
         <div className='header-container'>
-            <PinNumber />
-            <UiHeader as='h3'>Setup Household</UiHeader>
+            {location.pathname === '/household' ? <PinNumber /> : <Notifications />}
+            <UiHeader as='h3'>
+               { location.pathname === '/household' ? 'Setup Household' : 'Dashboard' }
+                </UiHeader>
             <AccountSettings />
         </div>
     )
