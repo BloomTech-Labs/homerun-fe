2385da0e40ef1a173d7fc98e4e7162a028d38ff0
MicahJank <MicahJank@users.noreply.github.com>
2020-03-05 11:39:26 -0600

    fixed errors that were causing the app to not compile

--- src/components/allSettings/AdultSettings.js
+++ src/components/allSettings/AdultSettings.js
@@ -1,39 +1,39 @@
-import React { Component } from 'react'
-import { Accordion, Icon } from ' semantic-ui-react'
+// import React, { Component } from 'react'
+// import { Accordion, Icon } from ' semantic-ui-react'
 
-import household from './Household.js';
+// import household from './Household.js';
 
-handleClick = (e, titleProps) => {
-    const { index } = titleProps
-    const {activeIndex} = this.state 
-    const newIndex = activeIndex === index ? -1 : index
+// handleClick = (e, titleProps) => {
+//     const { index } = titleProps
+//     const {activeIndex} = this.state 
+//     const newIndex = activeIndex === index ? -1 : index
 
-    this.setState({ activeIndex: newIndex })
-}
+//     this.setState({ activeIndex: newIndex })
+// }
 
-render() {
-    const { activeIndex } = this.state
+// render() {
+//     const { activeIndex } = this.state
     
-    return(
-        <Accordion>
-            <Accordion.Title
-            active={activeIndex === 0}
-            index ={00}
-            onClick={this.handleClick}
-            >
-                <Icon name= 'dropdown' />
-                Household Members
-            </Accordion.Title>
+//     return(
+//         <Accordion>
+//             <Accordion.Title
+//             active={activeIndex === 0}
+//             index ={00}
+//             onClick={this.handleClick}
+//             >
+//                 <Icon name= 'dropdown' />
+//                 Household Members
+//             </Accordion.Title>
 
-            <Accordion.Content active={activeIndex === 0}>
+//             <Accordion.Content active={activeIndex === 0}>
 
-            </Accordion.Content>
-        </Accordion>
+//             </Accordion.Content>
+//         </Accordion>
 
-    )
-}
-
-
+//     )
+// }
 
 
-export default AdultSettings;
+
+
+// export default AdultSettings;
--- src/components/dashboard/TodoList.js
+++ src/components/dashboard/TodoList.js
@@ -11,31 +11,31 @@ import SwipeLeft from './SwipeLeft.js';
 import Todo from './Todo.js';
 
 
-const TodoList= () => (
-  <List size='massive' divided verticalAlign='middle'>
-    <SwipeableList>
-      <SwipeableListItem
-      swipeLeft = {{
-        content: <SwipeLeft />,
-        action: () => console.log("Worked!")
-      }}
-      swipeRight={{
-        content: <div>SwipeRight</div>,
-        action: () => console.log('swiping')
-      }}
-      >
-        <Todo task={'Task 1'} />
-      </SwipeableListItem>
-      
-      <Todo task={'Task 2'} />
-      <Todo task={'Task 3'} />
-    </SwipeableList>
-  </List>
-)
+const TodoList= () => {
 
-    </SwipeableList>
-  </List>
-)
+  return (
+    <List size='massive' divided verticalAlign='middle'>
+      <SwipeableList>
+        <SwipeableListItem
+        swipeLeft = {{
+          content: <SwipeLeft />,
+          action: () => console.log("Worked!")
+        }}
+        swipeRight={{
+          content: <div>SwipeRight</div>,
+          action: () => console.log('swiping')
+        }}
+        >
+          <Todo task={'Task 1'} />
+        </SwipeableListItem>
+        
+        <Todo task={'Task 2'} />
+        <Todo task={'Task 3'} />
+      </SwipeableList>
+    </List>
+    )
+
+  }
 
 
 export default TodoList;
