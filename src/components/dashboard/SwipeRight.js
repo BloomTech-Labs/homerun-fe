0fe21ee48280e39253e6178590182a6018069e6e
MicahJank <MicahJank@users.noreply.github.com>
2020-03-04 23:00:17 -0600

    created a SwipeRight component

---
+++ src/components/dashboard/SwipeRight.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { List, Icon } from 'semantic-ui-react'; 
+
+
+
+const SwipeRight = () => {
+    
+    return (
+        <List.Item>
+            <Icon aria-hidden="true" className="checkmark" size='massive' />
+        </List.Item>
+    )
+}
+
+export default SwipeRight;
