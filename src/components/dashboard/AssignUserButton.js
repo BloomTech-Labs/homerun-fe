2f4a9f137747107f129fc2a25592a1fb49cbe99d
MicahJank <MicahJank@users.noreply.github.com>
2020-03-11 12:07:31 -0500

    trying to make the assign member button a drop down

--- src/components/dashboard/AssignUserButton.js
+++ src/components/dashboard/AssignUserButton.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-import { Icon, Button } from 'semantic-ui-react';
+import { Icon, Button, Dropdown } from 'semantic-ui-react';
 
 const AssignUserButton = () => {
-
+    // hard coding options for now but once we can get the endpoint for getting household members the options part will need to be updated
+    const options = [
+        { key: 'memberId', text: 'Mom', value: 'Mom' },
+        { key: 'memberId', text: 'Dad', value: 'Dad' },
+        { key: 'memberId', text: 'Daughter', value: 'Daughter' },
+        { key: 'memberId', text: 'Son', value: 'Son' },
+    ]
     return (
-        <Button circular icon='add user' />
+        <Dropdown button multiple options={options} className='assign-user' icon='add user' />
     )
 }
 
