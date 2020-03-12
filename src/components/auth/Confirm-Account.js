b58ed56672e7d5a8f3e270e2104aee280e151b79
Vinni Hoke <vinni.hoke@gmail.com>
2020-03-12 16:26:24 -0700

    Working through an array error

--- src/components/auth/Confirm-Account.js
+++ src/components/auth/Confirm-Account.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Loader, Dimmer } from "semantic-ui-react";
 
--- src/components/dashboard/Todo.js
+++ src/components/dashboard/Todo.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { List, Icon, Input, Modal, Button, Dropdown} from 'semantic-ui-react';
+import { List, Icon, Input, Modal, Button, Dropdown, Label } from 'semantic-ui-react';
 
 import '@sandstreamdev/react-swipeable-list/dist/styles.css';
 import { SwipeableListItem } from '@sandstreamdev/react-swipeable-list';
@@ -14,43 +14,54 @@ import { DeleteTodoModal } from './DeleteTodoModal.js';
 
 const Todo = props => {
     const [modalOpen, setModalOpen] = useState(false);
+    let assigned = []
 
-    console.log(props);
+    const handleAssignee = e => {
+        let item = e.target.value
+        if (assigned.includes(item)) {
+            let idx = assigned.indexOf(item)
+            assigned.splice(idx, 1)
+        } else {
+            assigned.push(item)
+        }
+    }
 
-    const options = [
-        { key: 'memberId', text: 'Mom', value: 'Mom' },
-        { key: 'memberId', text: 'Dad', value: 'Dad' },
-        { key: 'memberId', text: 'Daughter', value: 'Daughter' },
-        { key: 'memberId', text: 'Son', value: 'Son' },
-    ]
+
     return (
         <>
-        <DeleteTodoModal modalOpen={modalOpen} setModalOpen={setModalOpen} />
-        <List.Item className='todo'>
-            <SwipeableListItem
-            threshold = {0.50}
-                swipeLeft={{
-                    content: <SwipeLeft />,
-                    action: () => setModalOpen(true)
-                }}
-                swipeRight={{
-                    content: <SwipeRight />,
-                    action: () => console.log('Working')
-                }}
-            >
+            <DeleteTodoModal modalOpen={modalOpen} setModalOpen={setModalOpen} />
+            <List.Item className='todo'>
+                <SwipeableListItem
+                    threshold={0.50}
+                    swipeLeft={{
+                        content: <SwipeLeft />,
+                        action: () => setModalOpen(true)
+                    }}
+                    swipeRight={{
+                        content: <SwipeRight />,
+                        action: () => console.log('Working')
+                    }}
+                >
                     <List.Content className='todo-container'>
-                        
+
                         <div className='todo-left'>
                             <List.Header as='a'>{props.task.title} </List.Header>
                             <List.Header as={'h5'}>Due {props.task.due}</List.Header>
                         </div>
                         <div className='todo-right'>
-                           
-                            <Assign />
+                            <div className="ui buttons">
+                                <select className="ui floating dropdown button" onChange={handleAssignee}>
+                                    <option value="a">A</option>
+                                    <option value="b">B</option>
+                                    <option value="c">C</option>
+                                    <option value="d">D</option>
+                                </select>
+                            </div>
+                            {console.log(assigned)}
                         </div>
                     </List.Content>
-            </SwipeableListItem>
-        </List.Item>
+                </SwipeableListItem>
+            </List.Item>
         </>
     )
 }
