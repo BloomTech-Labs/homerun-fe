65025b7fd5cf2343a84c3a5a67dca2446af03b77
Mayankho <yankhot96@gmail.com>
2020-03-11 23:51:25 -0400

    Time calendar added no style yet

--- src/components/dashboard/todo-form/TodoForm.js
+++ src/components/dashboard/todo-form/TodoForm.js
@@ -1,8 +1,13 @@
-import React from "react";
-
+import React, {useState} from "react";
+import DatePicker from "react-datepicker";
+import dayjs from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat"
 import { Form, Input, Icon, Button, Modal, Header, Dropdown as SemanticDropDown } from "semantic-ui-react";
+import "react-datepicker/dist/react-datepicker.css";
 
 import "../../../SASS/TaskForm.scss";
+import "../../../SASS/TodoList.scss";
+dayjs.extend(advancedFormat);
 
 const options = [
   {icon: 'member', text: 'Mom' , value: 'Mom'},
@@ -11,6 +16,8 @@ const options = [
   {icon: 'member', text: 'Son' , value: 'Son'},
 
 ]
+
+
 
 const ModalButton = () => {
   return (
@@ -57,32 +64,51 @@ const AssignMember = () => (
 )
 
 
-const AssignTime = () => (
-  <Modal trigger={timeModal()}>
-  <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-header">
-    <h3 id="myModalLabel">Date/Time Picker</h3>
-  </div>
-  <div class="modal-body">
-    <div id="datetimepicker1" class="input-append date">
-      <input data-format="dd/MM/yyyy hh:mm:ss" type="text"></input>
-      <span class="add-on"><i data-time-icon="icon-time" data-date-icon="icon-calendar"></i></span>
-    </div>
-  </div>
-  <div class="modal-footer">
-    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-  </div>
+// const AssignTime = () => (
+//   <Modal trigger={timeModal()}>
+//   <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+//   <div class="modal-header">
+//     <h3 id="myModalLabel">Date/Time Picker</h3>
+//   </div>
+//   <div class="modal-body">
+//     <div id="datetimepicker1" class="input-append date">
+//       <input data-format="dd/MM/yyyy hh:mm:ss" type="text"></input>
+//       <span class="add-on"><i data-time-icon="icon-time" data-date-icon="icon-calendar"></i></span>
+//     </div>
+//   </div>
+//   <div class="modal-footer">
+//     <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+//   </div>
 
   
-</div>
+// </div> 
+//   </Modal>
+// )
 
-
-        
+const AssignTime = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const futureDate = dayjs(startDate.toString()).format("x");
+  console.log(<DatePicker />, "This is the date picker")
+  return (
+  <Modal trigger={timeModal()}>
+    <Modal.Header>Set Time</Modal.Header>
+    <Modal.Content datepicker>
+      <Modal.Description>
+      <DatePicker 
+      wrapped size = "medium"
+      className = "date-picker"
+      selected = {startDate}
+      onChange = {date => setStartDate(date)}
+      showTimeSelect
+      timeFormat="HH:mm"
+      timeIntervals={15}
+      minDate={new Date()}
+      timeCaption="time"
+      dateFormat ="MMMM d, yyyy h:mm aa"
+       />
+      </Modal.Description> 
+    </Modal.Content>
   </Modal>
-
-
-
-
-)
+)}
 
 export default TodoForm;
