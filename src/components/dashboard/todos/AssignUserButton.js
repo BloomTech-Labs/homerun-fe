ddaf122e95be0668d71c74067746e06665d7c63b
MicahJank <MicahJank@users.noreply.github.com>
2020-03-12 20:15:22 -0500

    updated the assign user button to use a modal

--- src/components/dashboard/todos/AssignUserButton.js
+++ src/components/dashboard/todos/AssignUserButton.js
@@ -1,27 +1,13 @@
 import React from 'react';
-
-import { Icon, Button, Dropdown } from 'semantic-ui-react';
+import { Icon, Button, Dropdown, Modal } from 'semantic-ui-react';
+import { AssignUserModal } from './AssignUserModal.js';
 
 const AssignUserButton = () => {
-    // hard coding options for now but once we can get the endpoint for getting household members the options part will need to be updated
-    const options = [
-        { key: '1', text: 'Mom', value: 'Mom' },
-        { key: '2', text: 'Dad', value: 'Dad' },
-        { key: '3', text: 'Daughter', value: 'Daughter' },
-        { key: '4', text: 'Son', value: 'Son' },
-    ]
+
     return (
-        <>
-        <Dropdown selection multiple options={options} icon='add user' />
-        {/* <div className='ui icon button'>
-            <select multiple='' className='ui dropdown fluid'>
-                <option value='mom'>Mom</option>
-                <option value='dad'>Dad</option>
-                <option value='daughter'>Daughter</option>
-            </select>
-        </div> */}
-      
-        </>
+        <Modal trigger={<Button icon='add user' />}>
+            <AssignUserModal />
+        </Modal>
     )
 }
 
---
+++ src/components/dashboard/todos/AssignUserModal.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Modal, Dropdown } from 'semantic-ui-react';
+
+export const AssignUserModal = () => {
+
+    // hard coding options for now but once we can get the endpoint for getting household members the options part will need to be updated
+    const options = [
+        { key: '1', text: 'Mom', value: 'Mom' },
+        { key: '2', text: 'Dad', value: 'Dad' },
+        { key: '3', text: 'Daughter', value: 'Daughter' },
+        { key: '4', text: 'Son', value: 'Son' },
+    ]
+
+    return (
+        <>
+        <Modal.Header> Assign a Member to a task</Modal.Header>
+          <Modal.Content>
+            <p>Some Stuff Here</p>
+            <Dropdown fluid multiple selection options={options} />
+          </Modal.Content>
+        </>
+
+    )
+}
