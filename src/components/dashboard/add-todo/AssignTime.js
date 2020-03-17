4b692fdfcc3a84f36970b5938f3d0b5a9fd1b36d
Mayankho <yankhot96@gmail.com>
2020-03-16 22:32:30 -0400

    imported dependencies

--- src/components/dashboard/add-todo/AssignTime.js
+++ src/components/dashboard/add-todo/AssignTime.js
@@ -1,25 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import dayjs from "dayjs";
 import "react-datepicker/dist/react-datepicker.css";
+import axiosWithAuth from "../../../utils/AxiosWithAuth.js";
+
 
 
 const AssignTime = () => {
     const [startDate, setStartDate] = useState(new Date());
     
-
     useEffect(() => {
         axiosWithAuth().put('/todos/a12345')
             .then(res => {
                 console.log(res.data)
                 const futureDate = dayjs(startDate.toString()).format("x");
-                setDate(res.data)
             })
             .catch(err => {
                 console.log(err);
             })
     }, []) 
-
     console.log(<DatePicker />, "This is the date picker")
     return (   
         <DatePicker 
