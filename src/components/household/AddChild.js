5844d9af2ce65edb471ef92172d9d3d254f2dd99
Vinni Hoke <vinni.hoke@gmail.com>
2020-03-19 18:53:47 -0700

    Setting up redux

--- src/components/household/AddChild.js
+++ src/components/household/AddChild.js
@@ -3,22 +3,22 @@ import { Button, Checkbox, Form } from 'semantic-ui-react';
 
 const AddChild = (props) => {
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    props.setChildModal(false)
-  }
-    return (
-            <Form>
-              <Form.Field>
-                <label>Name</label>
-                <input placeholder='Member' />
-              </Form.Field>
-              <Form.Field>
-                <Checkbox label='Child?' />
-              </Form.Field>
-              <Button onClick={onSubmit} type='submit'>Add</Button>
-            </Form>
-          )
+	const onSubmit = (e) => {
+		e.preventDefault();
+		props.setChildModal(false)
+	}
+	return (
+		<Form>
+			<Form.Field>
+				<label>Name</label>
+				<input placeholder='Member' />
+			</Form.Field>
+			<Form.Field>
+				<Checkbox label='Child?' />
+			</Form.Field>
+			<Button onClick={onSubmit} type='submit'>Add</Button>
+		</Form>
+	)
 }
 
-export default AddChild;
+export default AddChild
