ad1ba5d94ec7292b23ea69575973b66b0b2f14e5
Zach Taylor <zbtaylor1@gmail.com>
2020-03-23 22:14:54 -0400

    addchild now grabbing data from backend, signin email no longer using hardcoded URL for post request

--- src/components/household/AddChild.js
+++ src/components/household/AddChild.js
@@ -1,22 +1,35 @@
 import React from "react";
-import { Button, Checkbox, Form } from 'semantic-ui-react';
-import { useForm } from 'react-hook-form';
-const AddChild = (props) => {
+import { Button, Checkbox, Form } from "semantic-ui-react";
+import { useForm } from "react-hook-form";
+import axiosWithAuth from "../../utils/AxiosWithAuth";
+const AddChild = props => {
   const { register, handleSubmit, errors } = useForm();
 
-  const onSubmit = (e) => {
+  const onSubmit = (data, e) => {
     e.preventDefault();
-    props.setModal(false)
-  }
-    return (
-            <Form>
-              <Form.Field>
-                <label>Name</label>
-                <input placeholder='Member' />
-              </Form.Field>
-              <Button onClick={onSubmit} floated='right' primary type='submit'>Add</Button>
-            </Form>
-          )
-}
+    axiosWithAuth()
+      .post("/members/household/children", data)
+      .then(res => {
+        console.log(res);
+        props.setModal(false);
+      });
+  };
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Form.Field>
+        <label>Name</label>
+        <input
+          name="username"
+          placeholder="Child's Name"
+          ref={register({ required: "Please give the child a name." })}
+        />
+        {errors.username && <p>{errors.username.message}</p>}
+      </Form.Field>
+      <Button floated="right" primary type="submit">
+        Add
+      </Button>
+    </Form>
+  );
+};
 
 export default AddChild;
--- src/components/household/List.js
+++ src/components/household/List.js
@@ -1,42 +1,62 @@
 import React, { useState, useEffect } from "react";
 import Name from "./Name";
 
-import axiosWithAuth from '../../utils/AxiosWithAuth.js';
+import axiosWithAuth from "../../utils/AxiosWithAuth.js";
 
 // Since this component itself is named List i had to import Semantic Ui's List component as UiList
-import { Button, Modal, Image, List as UiList, Loader, Confirm } from 'semantic-ui-react';
-import AddChild from './AddChild.js';
-import InviteMember from './InviteMember.js';
+import {
+  Button,
+  Modal,
+  Image,
+  List as UiList,
+  Loader,
+  Confirm
+} from "semantic-ui-react";
+import AddChild from "./AddChild.js";
+import InviteMember from "./InviteMember.js";
 
 const List = () => {
- const [childModal, setChildModal] = useState(false);
- const [memberModal, setMemberModal] = useState(false);
- const [members, setMembers] = useState([]); 
+  const [childModal, setChildModal] = useState(false);
+  const [memberModal, setMemberModal] = useState(false);
+  const [members, setMembers] = useState([]);
 
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/members/household/assignable")
+      .then(res => {
+        console.log("List -> res", res);
+        setMembers(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
 
- useEffect(() => {
-    axiosWithAuth().get('/members/household/assignable')
-        .then(res => {
-        console.log("List -> res", res)
-            setMembers(res.data);
-        })
-        .catch(err => {
-            console.log(err);
-        })
- }, [])
-
-        return (
-            <div>
-                <UiList selection verticalAlign='middle'>
-                    {members.map(member => {
-                        return <Name key={member.id} name={member.username} />
-                    })}
-                </UiList>
-                <Modal open={memberModal} onClose={() => setMemberModal(false)} trigger={<Button onClick={() => setMemberModal(true)}>Invite Member</Button>} header='Invite a user to your household' content={<InviteMember setModal={setMemberModal} />}></Modal>
-                <Modal open={childModal} onClose={() => setChildModal(false)} trigger={<Button onClick={() => setChildModal(true)}>Add Child</Button>} header='Add your child' content={<AddChild setModal={setChildModal} />}></Modal>
-            </div>
-        )
-    }
-
+  return (
+    <div>
+      <UiList selection verticalAlign="middle">
+        {members.map(member => {
+          return <Name key={member.username} name={member.username} />;
+        })}
+      </UiList>
+      <Modal
+        open={memberModal}
+        onClose={() => setMemberModal(false)}
+        trigger={
+          <Button onClick={() => setMemberModal(true)}>Invite Member</Button>
+        }
+        header="Invite a user to your household"
+        content={<InviteMember setModal={setMemberModal} />}
+      ></Modal>
+      <Modal
+        open={childModal}
+        onClose={() => setChildModal(false)}
+        trigger={<Button onClick={() => setChildModal(true)}>Add Child</Button>}
+        header="Add your child"
+        content={<AddChild setModal={setChildModal} />}
+      ></Modal>
+    </div>
+  );
+};
 
 export default List;
