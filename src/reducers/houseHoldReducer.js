f0eaa00e22cbd70822ea2aaad6981aa14c4a5fab
MicahJank <MicahJank@users.noreply.github.com>
2020-03-25 21:47:46 -0500

    added the houseHoldReducer and imported it into the reducer index.js

---
+++ src/reducers/houseHoldReducer.js
@@ -0,0 +1,38 @@
+import { FETCH_MEMBERS_FAIL, FETCH_MEMBERS_SUCCESS, FETCH_MEMBERS_START } from '../actions/houseHoldActions.js';
+
+const initialState = {
+    household: {
+      members: [],
+      error: '',
+      loadingMembers: false
+    }
+  };
+  
+  const houseHoldReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_MEMBERS_START:
+            return {
+            ...state,
+            error: '',
+            loadingMembers: true
+            }
+        case FETCH_MEMBERS_SUCCESS:
+            return {
+                ...state,
+                error: '',
+                loadingMembers: false,
+                members: action.payload
+            }
+        case FETCH_MEMBERS_FAIL:
+            return {
+                ...state,
+                error: action.payload,
+                loadingMembers: false
+            } 
+      default:
+        return state;
+    }
+  };
+  
+  export default houseHoldReducer;
+  
