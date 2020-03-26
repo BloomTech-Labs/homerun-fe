2b00bc7db46cd403895944619d3bd69a59ed04ca
MicahJank <MicahJank@users.noreply.github.com>
2020-03-25 21:37:43 -0500

    added the houseHold action to the actions index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 // Import any group of actions pertaining to a specific task
 import userActions from "./userActions.js";
+import houseHoldActions from './houseHoldActions.js';
 
 const actions = {
   // Add any actions here.
-  user: userActions
+  user: userActions,
+  houseHold: houseHoldActions
 };
 
 export default actions;
