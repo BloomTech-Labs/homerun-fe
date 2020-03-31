d00e25f324dfc4c3dba8fcd4b12a052439a0de9b
MicahJank <MicahJank@users.noreply.github.com>
2020-03-30 21:42:16 -0500

    invite member has been refactored into redux

--- src/actions/houseHoldActions.js
+++ src/actions/houseHoldActions.js
@@ -4,6 +4,7 @@ export const LOADING = 'LOADING';
 export const FETCH_MEMBERS_SUCCESS = 'FETCH_MEMBERS_SUCCESS';
 export const ERROR = 'ERROR';
 export const ADD_CHILD = 'ADD_CHILD';
+export const INVITE_MEMBER = 'INVITE_MEMBER';
 
 // Action Creators
 const fetchHousehold = () => dispatch => {
@@ -19,7 +20,7 @@ const fetchHousehold = () => dispatch => {
         .catch(err => {
             dispatch({
                 type: ERROR,
-                payload: err
+                payload: err.response.data.message || err
             })
         })
   };
@@ -37,13 +38,28 @@ const fetchHousehold = () => dispatch => {
     .catch(err => {
         dispatch({
             type: ERROR,
-            payload: err
+            payload: err.response.data.message || err
         })
+    })
+  }
+
+  // setModal is the function that turns the invite user modal on or off - it takes a boolean value as its argument
+  const inviteMember = (data, setModal) => dispatch => {
+    dispatch({ type: LOADING });
+    axiosWithAuth().post('/members/household/invite', data)
+    .then(res => {
+      console.log("onSubmit -> res", res)
+      setModal(false);
+    })
+    .catch(err => {
+      dispatch({ type: ERROR, payload: err.response.data.message || err })
+      console.log(err);
     })
   }
   
   export default {
     fetchHousehold,
-    addChild
+    addChild,
+    inviteMember
   };
   
--- src/components/household/InviteMember.js
+++ src/components/household/InviteMember.js
@@ -1,35 +1,36 @@
 import React, { useState, useEffect } from "react";
 import {Button, Icon, Header, Form, Container, Loader, Dimmer } from "semantic-ui-react";
 import { useForm } from "react-hook-form";
-import axiosWithAuth from '../../utils/AxiosWithAuth.js';
+import { useDispatch, useSelector } from 'react-redux';
+import actions from '../../actions';
+import "antd/lib/alert/style/css";
+import '../../SASS/InviteMember.scss';
+
+import { Alert } from 'antd';
 
 const InviteMember = (props) => {
-  const { register, handleSubmit, errors } = useForm();
-  const [isLoading, setIsLoading] = useState(false);
+  const { register, handleSubmit} = useForm();
+  const dispatch = useDispatch();
+  const stateError = useSelector(state => state.household.error)
+  const loadingState = useSelector(state => state.household.loading);
 
   const onSubmit = (data) => {
-    setIsLoading(true);
-    console.log(data);
-    const axiosURL = props.inviteMember ?  '/members/household/invite' : props.addChild ? '/members/household/children' : null;
-    axiosWithAuth().post(axiosURL, data)
-      .then(res => {
-        console.log("onSubmit -> res", res)
-        
-        props.setModal(false);
-      })
-      .catch(err => {
-        console.log(err);
-      })
+    dispatch(actions.houseHold.inviteMember(data, props.setModal));
   }
-    return isLoading ? ( <Dimmer active inverted><Loader size="large">Loading</Loader></Dimmer>)
-                     : (<Form onSubmit={handleSubmit(onSubmit)}>
+  console.log(stateError)
+    return loadingState ? ( <Dimmer active inverted><Loader size="large">Loading</Loader></Dimmer>)
+                     : (
+                        <>
+                        <Form onSubmit={handleSubmit(onSubmit)}>
                           <Form.Field>
                             <label>Email</label>
-                            <input type='email' name='email' placeholder='user@email.com' ref={register({ required: "Email is invalid." })} />
-                            {errors.email && <p>{errors.email.message}</p>}
+                            <input type='email' name='email' placeholder='user@email.com' ref={register} />
+                            {stateError && <p>{stateError}</p>}
                           </Form.Field>
                           <Button floated='right' primary type='submit'>Invite</Button>
-                        </Form>)
+                        </Form>
+                       </>
+                        )
           
 }
 
