61a78bf6bfa2c68068638a537b2fbda733e959b2
MicahJank <MicahJank@users.noreply.github.com>
2020-04-06 14:03:04 -0500

    updated household settings to have a dropdown and used the householdmembers state to populate the dropdown

--- src/components/settings/HouseholdSettings.js
+++ src/components/settings/HouseholdSettings.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { Dropdown, Icon, Loader, Dimmer } from 'semantic-ui-react';
+
 
 const HouseholdSettings = () => {
-  return <div>Household Settings</div>;
+  const members = useSelector(state => state.household.members);
+  const currentUser = useSelector(state => state.users);
+  console.log("HouseholdSettings -> currentUser", currentUser)
+
+const trigger = (<span> <Icon name='user' /> Hello, {currentUser.username}</span>);
+  const dropdownOptions = members.map(member => {
+    return { key: member.id, text: member.username, value: member.username };
+  });
+  console.log("HouseholdSettings -> dropdownOptions", dropdownOptions)
+  
+  return (
+    <div>
+      <h3>Household Settings</h3>
+      <Dropdown trigger={trigger} options={dropdownOptions} />
+    </div>
+    );
 };
 
 export default HouseholdSettings;
