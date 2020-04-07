8d30ac62ce8f1186bf953037c620f79d3c31d035
MicahJank <MicahJank@users.noreply.github.com>
2020-04-06 20:00:26 -0500

    updated user actions for sidebar dropdown

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,8 +1,31 @@
+import axios from 'axios';
+
 // Action Creators
 const setUser = (user) => dispatch => {
 	return dispatch({ type: "SET_USER", payload: user })
 }
 
+const changeUser = (user) => dispatch => {
+  const { email, password } = user;
+  if(!user.child) {
+    axios
+      .post(`${process.env.REACT_APP_BE_URL}/auth/login`, {email, password })
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.token);
+        setUser(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  } else  {
+    setUser(user);
+  }
+  
+  // return dispatch({ type: "CHANGE_USER", payload: user})
+}
+
 export default {
-  setUser
+  setUser,
+  changeUser
 };
--- src/components/settings/HouseholdSettings.js
+++ src/components/settings/HouseholdSettings.js
@@ -1,24 +1,58 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
-import { Dropdown, Icon, Loader, Dimmer } from 'semantic-ui-react';
+import { Dropdown, Icon, Loader, Dimmer, Modal, Input, Button } from 'semantic-ui-react';
 
+import axios from 'axios';
+import actions from "../../actions";
 
 const HouseholdSettings = () => {
   const members = useSelector(state => state.household.members);
-  console.log("HouseholdSettings -> members", members)
   const currentUser = useSelector(state => state.users);
-  console.log("HouseholdSettings -> currentUser", currentUser)
+  const dispatch = useDispatch();
+  const [dropDownValue, setDropDownValue] = useState(currentUser.username);
+  const [modal, setModal] = useState(false);
+  const [password, setPassword] = useState('');
 
-const trigger = (<span> <Icon name='user' /> Hello, {currentUser.username}</span>);
   const dropdownOptions = members.map(member => {
-    return { key: member.id, text: member.username, value: member.username, disabled: member.child ? false : true };
+    return { key: member.id, text: member.username, value: member.username }; 
   });
-  console.log("HouseholdSettings -> dropdownOptions", dropdownOptions)
   
+  const handleChange = (event, { value }) => {
+    event.persist();
+    if(event.target.type === 'password') {
+      setPassword(event.target.value);
+    } else {
+      setDropDownValue(value);
+      const [user] = members.filter(user => value === user.username);
+      if(!user.child) { // no need to put in a password for a child user
+        setModal(true);
+      } else {
+        dispatch(actions.user.changeUser(user))
+      }
+    }
+  }
+
+  const handleSubmit = event => {
+    const [user] = members.filter(user => dropDownValue === user.username);
+    event.preventDefault();
+    dispatch(actions.user.changeUser({...user, password}))
+  }
+
   return (
     <div>
       <h3>Household Settings</h3>
-      <Dropdown trigger={trigger} options={dropdownOptions} />
+      <Dropdown selection onChange={handleChange} placeholder={`Welcome, ${currentUser.username}`} value={dropDownValue} fluid options={dropdownOptions} />
+      <Modal
+        open={modal}
+        onClose={() => setModal(false)}
+        header="Please enter that users password to continue"
+        content={(
+          <>
+        <Input onChange={handleChange} value={password} type='password' fluid placeholder={'Enter Password...'}/>
+        <Button onClick={handleSubmit} content='Submit' />
+        </>
+        )}
+      ></Modal>
     </div>
     );
 };
