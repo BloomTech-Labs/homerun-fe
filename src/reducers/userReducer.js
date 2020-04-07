a2b62dea99185c254f429fbdf943f6a4cd16d3a8
MicahJank <MicahJank@users.noreply.github.com>
2020-04-07 15:32:45 -0500

    updated the userReducer to  be able to have a loading state

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,12 @@
-const userReducer = (state = {}, action) => {
+const intialState = {
+  loading: false,
+  userInfo: {}
+}
+
+const userReducer = (state = intialState, action) => {
   switch (action.type) {
     case "SET_USER":
-      return action.payload;
+      return {...state, userInfo: action.payload };
     default:
       return state;
   }
