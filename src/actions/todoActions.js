7f826e4a6b4c6de84557a979d843f703cca2a8ab
Zach Taylor <zbtaylor1@gmail.com>
2020-04-10 17:04:41 -0400

    removed fetch assigned action as they come with the todos from be

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -1,40 +1,34 @@
-import axiosWithAuth from '../utils/AxiosWithAuth.js'
+import axiosWithAuth from "../utils/AxiosWithAuth.js";
 
 // Action Creators
-const fetchTodos = () => dispatch => {
-	axiosWithAuth().get(`/todos/household`)
-		.then(res => dispatch({ type: "FETCH_TODOS", payload: res.data }))
-		// .then(res => console.log(res.data))
-		.catch(err => console.log(err.message))
-}
-const addTodo = (todo) => dispatch => {
-	axiosWithAuth().post(`/todos/add`, todo)
-		.then(res => dispatch({ type: "ADD_TODO", payload: res.data }))
-		// .then(res => console.log(res.data))
-		.catch(err => console.log(err.message))
-}
+const fetchTodos = () => (dispatch) => {
+  axiosWithAuth()
+    .get(`/todos/household`)
+    .then((res) => dispatch({ type: "FETCH_TODOS", payload: res.data }))
+    // .then(res => console.log(res.data))
+    .catch((err) => console.log(err.message));
+};
 
-const removeTodo = (todoId) => dispatch => {
-	axiosWithAuth().delete(`/todos/${todoId}`)
-		.then(res => {
-			dispatch({ type: "REMOVE_TODO", payload: res.data });
-		})
-		.catch(err => console.log(err.message))
-}
+const addTodo = (todo) => (dispatch) => {
+  axiosWithAuth()
+    .post(`/todos/add`, todo)
+    .then((res) => dispatch({ type: "ADD_TODO", payload: res.data }))
+    // .then(res => console.log(res.data))
+    .catch((err) => console.log(err.message));
+};
 
-const fetchAssignedUsers = (todoID) => dispatch => {
-	axiosWithAuth().get(`/todos/assigned/${todoID}`)
-		.then(res => {
-			console.log(res);
-		})
-		.catch(err => {
-			console.log(err);
-		})
-}
+const removeTodo = (todoId) => (dispatch) => {
+  axiosWithAuth()
+    .delete(`/todos/${todoId}`)
+    .then((res) => {
+      dispatch({ type: "REMOVE_TODO", payload: res.data });
+    })
+    .catch((err) => console.log(err.message));
+};
 
 export default {
-	fetchTodos,
-	addTodo,
-	removeTodo,
-	fetchAssignedUsers
-}
+  fetchTodos,
+  addTodo,
+  removeTodo,
+  fetchAssignedUsers,
+};
