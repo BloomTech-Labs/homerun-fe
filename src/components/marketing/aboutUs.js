0c6df06642616c8d536492810335fc271001e65d
Mayankho <yankhot96@gmail.com>
2020-04-13 21:12:19 -0400

    About us HTMl markup start

--- src/components/marketing/aboutUs.js
+++ src/components/marketing/aboutUs.js
@@ -8,22 +8,31 @@ import semantic from "semantic-ui-react";
 const aboutUs = () => {
     return(
         <segment>
+            <nav>
+                <navlink>Home</navlink>
+                <navlink>Contact Us</navlink>
+                <navlink>Abou Us</navlink>
+            </nav>
             <div className = "Total-content-controller">
             <div class="ui four column doubling stackable grid container">
   <div class="column">
-      <h1>About US</h1>
+      <h1>Vinnie</h1>
     <p></p>
     <p></p>
   </div>
   <div class="column">
+      <h1>Heather</h1>
     <p></p>
     <p></p>
   </div>
   <div class="column">
+      <h1>Micah</h1>
+
     <p></p>
     <p></p>
   </div>
   <div class="column">
+      <h1>Yankho</h1>
     <p></p>
     <p></p>
   </div>
