7410d1f0a7a657ad950e43fb7c0c27216284091e
Zach Taylor <zbtaylor1@gmail.com>
2020-04-16 23:34:59 -0400

    updated account and created a basic account form placeholder file

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import { Row, Col } from "antd";
 import ChildAccountDropdown from "./ChildAccountDropdown";
 
---
+++ src/components/account/AccountForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Button, Form, Container } from "semantic-ui-react";
+import { useForm } from "react-hook-form";
+
+const AccountForm = () => {};
+
+export default AccountForm;
