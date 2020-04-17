bfd24f61c61db0437cb30a0015b161f094a60ec2
Zach Taylor <zbtaylor1@gmail.com>
2020-04-16 23:21:27 -0400

    renamed householdsettings to childaccountdropdown, moved it, and deleted account settings

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { Row, Col } from "antd";
+import ChildAccountDropdown from "./ChildAccountDropdown";
 
 const Account = () => {
   return (
     <div className="account container">
       <Row style={{ margin: "50px 0" }}>
         <Col span={24}>
-          <NavLink className="dashboard-btn" to="/dashboard">
-            <h2>Account Stuff</h2>
-          </NavLink>
+          <ChildAccountDropdown />
         </Col>
       </Row>
     </div>
---
+++ src/components/account/ChildAccountDropdown.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Dropdown } from "semantic-ui-react";
+import actions from "../../actions";
+
+const ChildAccountDropdown = () => {
+  const members = useSelector((state) => state.household.members);
+  const currentUser = useSelector((state) => state.user.userInfo);
+  const dispatch = useDispatch();
+  const [dropDownValue, setDropDownValue] = useState();
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    const children = members
+      .filter((member) => member.child)
+      .map((child) => {
+        return { key: child.id, text: child.username, value: child.username };
+      });
+
+    setOptions(children);
+  }, []);
+
+  const handleChange = (event, { value }) => {
+    event.persist();
+    setDropDownValue(value);
+    const [user] = members.filter((user) => value === user.username);
+    dispatch(actions.user.setChild(user));
+  };
+
+  return (
+    <div>
+      <h3>Children:</h3>
+      <Dropdown
+        selection
+        onChange={handleChange}
+        placeholder={`Please select a child.`}
+        value={dropDownValue}
+        fluid
+        options={options}
+      />
+    </div>
+  );
+};
+
+export default ChildAccountDropdown;
