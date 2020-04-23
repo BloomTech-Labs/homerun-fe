6ec4883f128ea51abe2e194758d4b1473f6bb3d7
Zach Taylor <zbtaylor1@gmail.com>
2020-04-22 22:00:10 -0400

    removed old dashboard link from household

--- src/components/household/Household.js
+++ src/components/household/Household.js
@@ -12,13 +12,6 @@ const Household = () => {
   return (
     <div className="household container">
       <List />
-      <Row style={{ margin: "50px 0" }}>
-        <Col span={24}>
-          <NavLink className="dashboard-btn" to="/dashboard">
-            <button className="ui button blue circular">Dashboard</button>
-          </NavLink>
-        </Col>
-      </Row>
     </div>
   );
 };
