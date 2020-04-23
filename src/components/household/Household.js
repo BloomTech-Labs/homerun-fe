e2e4df37d701931da209de9865cacaea688d4402
Zach Taylor <zbtaylor1@gmail.com>
2020-04-22 22:01:08 -0400

    cleaned up unused imports

--- src/components/household/Household.js
+++ src/components/household/Household.js
@@ -1,12 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import React from "react";
 import List from "./List";
-
 import "../../SASS/Household.scss";
-
-import { Button, Icon } from "semantic-ui-react";
-import { NavLink, Redirect } from "react-router-dom";
-import { Row, Col } from "antd";
 
 const Household = () => {
   return (
