462fefc2ea09b696ea634221afac5aaed0fd1531
Micah <micahjank@gmail.com>
2020-04-27 14:19:25 -0500

    deleted the home testing file as it is not needed and was causing errors

--- src/components/dashboard/Categories/Tab.js
+++ src/components/dashboard/Categories/Tab.js
@@ -5,9 +5,7 @@ import { Badge } from 'antd';
 
 const Tab = (props) => (
                 <Menu.Item name={props.name} active={props.active} onClick={props.handleClick}>
-                    <Badge offset={[10, 0]} count={props.counts}>
                         {props.name.toUpperCase()}
-                    </Badge>
                 </Menu.Item>
     )
 
