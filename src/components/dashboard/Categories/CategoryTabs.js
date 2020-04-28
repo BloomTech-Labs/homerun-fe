2588342b72e0140b25b5d1f26c2efec5dd1a2a2e
Zach Taylor <zbtaylor1@gmail.com>
2020-04-27 21:37:40 -0400

    added category-tabs classname back in

--- src/components/dashboard/Categories/CategoryTabs.js
+++ src/components/dashboard/Categories/CategoryTabs.js
@@ -1,16 +1,16 @@
 import React, { useState, useLayoutEffect } from "react";
 
-import { useDispatch, useSelector } from 'react-redux';
-import actions from '../../../actions/';
-import { Menu } from 'semantic-ui-react';
+import { useDispatch, useSelector } from "react-redux";
+import actions from "../../../actions/";
+import { Menu } from "semantic-ui-react";
 
-import Tab from './Tab.js';
+import Tab from "./Tab.js";
 
-const CategoryTabs = () => {    
-    const [active, setActive] = useState('all')
-    const [counts, setCounts] = useState({});
-    const dispatch = useDispatch();
-    const state = useSelector(state => state.todos.todos);
+const CategoryTabs = () => {
+  const [active, setActive] = useState("all");
+  const [counts, setCounts] = useState({});
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.todos.todos);
 
   const handleClick = (e, { name }) => {
     e.preventDefault();
@@ -20,24 +20,58 @@ const CategoryTabs = () => {
     setActive(name);
   };
 
-    useLayoutEffect(() => {
-        if(state) {
-       setCounts({
-            living_room: state.filter(todo => todo.categories.includes('living_room')).length,
-            bedroom: state.filter(todo => todo.categories.includes('bedroom')).length,
-            kitchen: state.filter(todo => todo.categories.includes('kitchen')).length,
-            bathroom: state.filter(todo => todo.categories.includes('bathroom')).length,
-        })}
-    }, [state])
+  useLayoutEffect(() => {
+    if (state) {
+      setCounts({
+        living_room: state.filter((todo) =>
+          todo.categories.includes("living_room")
+        ).length,
+        bedroom: state.filter((todo) => todo.categories.includes("bedroom"))
+          .length,
+        kitchen: state.filter((todo) => todo.categories.includes("kitchen"))
+          .length,
+        bathroom: state.filter((todo) => todo.categories.includes("bathroom"))
+          .length,
+      });
+    }
+  }, [state]);
 
-    return (<div>
-             <Menu pointing secondary>
-                 <Tab name='all' active={active === 'all'} handleClick={handleClick} counts={0} />
-                 <Tab name='living_room' active={active === 'living_room'} handleClick={handleClick} counts={counts.living_room} />
-                 <Tab name='bedroom' active={active === 'bedroom'} handleClick={handleClick} counts={counts.bedroom} />
-                 <Tab name='kitchen' active={active === 'kitchen'} handleClick={handleClick} counts={counts.kitchen} />
-                 <Tab name='bathroom' active={active === 'bathroom'} handleClick={handleClick} counts={counts.bathroom} />
-             </Menu>
-        </div>)}
+  return (
+    <div className="category-tabs">
+      <Menu pointing secondary>
+        <Tab
+          name="all"
+          active={active === "all"}
+          handleClick={handleClick}
+          counts={0}
+        />
+        <Tab
+          name="living_room"
+          active={active === "living_room"}
+          handleClick={handleClick}
+          counts={counts.living_room}
+        />
+        <Tab
+          name="bedroom"
+          active={active === "bedroom"}
+          handleClick={handleClick}
+          counts={counts.bedroom}
+        />
+        <Tab
+          name="kitchen"
+          active={active === "kitchen"}
+          handleClick={handleClick}
+          counts={counts.kitchen}
+        />
+        <Tab
+          name="bathroom"
+          active={active === "bathroom"}
+          handleClick={handleClick}
+          counts={counts.bathroom}
+        />
+      </Menu>
+    </div>
+  );
+};
 
 export default CategoryTabs;
