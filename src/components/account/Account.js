46ceecc321cb366481aefe296e1c7ac115f7c41d
Katrina Roaix <kroaix@gmail.com>
2020-04-29 20:45:39 -0400

    pulled from master and changed homepage content

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -1,28 +1,14 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
 import { Row, Col } from "antd";
 import ChildAccountDropdown from "./ChildAccountDropdown";
-import { useDispatch } from "react-redux";
-import actions from "../../actions/index";
 
-const Account = (props) => {
-  const dispatch = useDispatch();
-
-  const handleDelete = (id) => {
-    dispatch(actions.user.deleteUser(id));
-    props.history.push(`/signup`);
-  }
-
+const Account = () => {
   return (
     <div className="account container">
       <Row style={{ margin: "50px 0" }}>
         <Col span={24}>
           <h3>Switch to a Child Account</h3>
           <ChildAccountDropdown />
-        </Col>
-        <Col span={24}>
-          <h3>Want to delete your account?</h3>
-          <Button onClick={handleDelete}>Delete</Button>
         </Col>
       </Row>
     </div>
