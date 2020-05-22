2d71159336445978e5d146b3e5efcc71c4fec240
tauan-longaretti <tauanlongaretti@gmail.com>
2020-05-22 02:04:43 -0400

    component is fully refactored with tailwind to meet Figma design and responsiveness

--- src/components/marketing/Home.js
+++ src/components/marketing/Home.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Icon } from "semantic-ui-react";
+import { Icon } from "semantic-ui-react";
 import hero from "../../Logos/hero.png";
 import Navigation from "../marketing/Navigation";
 import Footer from "../marketing/Footer";
-import { Row, Col } from "antd";
 
 // right now the button takes us to the email sign up but from the Home page the next step would actually be the initial sign in page where the user can choose to sign in with
 // google or email - once those components are built out we can have the navlink take us to that route
@@ -12,48 +11,44 @@ const Home = () => {
   return (
     <>
       <Navigation />
-      <main className="px-20 py-24">
-        <div className="flex justify-center">
-          <img
-            className=""
-            src={hero}
-            alt="icon of desktop, tablet and cellphone"
-          />
+      <main className="px-20 py-24 desktop:flex desktop:px-28 desktop:py-48">
+        <div className="flex justify-center desktop:w-1/2 desktop:justify-end desktop:pr-10">
+          <img src={hero} alt="icon of desktop, tablet and cellphone" />
         </div>
-        <div className="flex justify-center mt-20">
-          <h1 className="text-center w-full max-w-2xl">
-            TidyHive is a home organizational tool where members of a household
-            can create, assign, and complete tasks. Turn your todos into todones
-            with TidyHive today!
-          </h1>
-        </div>
-        <div className="flex justify-center mt-20">
-          <button className="bg-hive font-bold h-20 w-full max-w-2xl py-4 px-10 border rounded">
-            <Link to="/signup" className="text-2xl text-white">
-              Signup for free <Icon name="arrow right" />
-            </Link>
-          </button>
+        <div className="desktop:w-1/2">
+          <div className="flex justify-center mt-20 desktop:justify-start desktop:pl-10 desktop:mt-5">
+            <h1 className="text-center w-full max-w-2xl phone: text-3xl tablet:text-4xl desktop:text-5x desktop:text-left desktop:max-w-xl">
+              <span className="text-hive">TidyHive</span> is a home
+              organizational tool where members of a household can create,
+              assign, and complete tasks. Turn your todos into todones with
+              TidyHive today!
+            </h1>
+          </div>
+          <div className="flex justify-center mt-20 desktop:justify-start desktop:pl-10 desktop:mt-10">
+            <button className="bg-hive shadow-lg font-bold h-20 w-full max-w-2xl py-4 px-6 border rounded desktop:w-2/5">
+              <Link to="/signup" className="text-2xl text-white">
+                Signup for free <Icon name="arrow right" />
+              </Link>
+            </button>
+          </div>
         </div>
       </main>
-
-      <section className="flex justify-center flex-wrap m-auto px-20">
-		<div className="">
-        <div className="text-center shadow p-10 w-full">
-          <Icon className="" name="list" size="massive" />
-          <h2 className="">Create Todos</h2>
+      <section className="px-20 max-w-full desktop:flex desktop:px-64">
+        <div className="text-center m-auto shadow-lg p-10 max-w-2xl desktop:px-20">
+          <Icon name="list" size="massive" />
+          <h2>Create Todos</h2>
         </div>
-        <div className="text-center mt-20 shadow p-10 w-full">
+        <div className="text-center m-auto mt-20 shadow-lg p-10  max-w-2xl desktop:mt-0 desktop:px-16">
           <Icon name="add user" size="massive" />
-          <h2 className="">Assign Members</h2>
+          <h2>Assign Members</h2>
         </div>
-        <div className="text-center mt-20 shadow p-10 w-full">
+        <div className="text-center m-auto mt-20 shadow-lg p-10 max-w-2xl desktop:mt-0 desktop:px-16">
           <Icon name="tasks" size="massive" />
-          <h2 className="">Complete Tasks</h2>
+          <h2>Complete Tasks</h2>
         </div>
-		</div>
       </section>
-      <section className="flex justify-center mt-20 mb-24 px-20">
-        <h2 className="text-center max-w-2xl">
+      <section className="flex justify-center mt-20 mb-24 px-20 desktop:m-40">
+        <h2 className="text-center max-w-2xl text-2xl tablet:text-3xl desktop:max-w-full desktop:px-16">
           These are the main features that TidyHive has to offer. Keep your
           household organized and everyone on the same page by creating tasks
           and assigning members to those tasks. Often times a majority of the
