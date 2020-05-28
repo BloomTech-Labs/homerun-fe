8d62fe58b7a6d9c6bdb82c7bf136dae878a18fbd
Batuhan Balta <bthn.balta@gmail.com>
2020-05-28 01:52:33 -0700

    contact-unit-test created.

---
+++ src/components/marketing/ContactUsForm.spec.js
@@ -0,0 +1,4 @@
+import ContactUsForm from "./ContactUsForm";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { render } from "@testing-library/react";
