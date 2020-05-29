a8beba5e882faa2a953a8e2b1bf9b4bde5f4e91d
tauan-longaretti <tauanlongaretti@gmail.com>
2020-05-29 16:54:46 -0400

    fixed mobile responsiveness of SignIn component

--- src/components/auth/SignIn-Landing.js
+++ src/components/auth/SignIn-Landing.js
@@ -34,13 +34,13 @@ const SignInLanding = (props) => {
 	return (
 		<>
 			<section className='flex flex-col items-center justify-center h-full'>
-				<div className='py-4 text-center'>
-					<h2 className='text-4xl text-gray-700 text-l bold'>
+				<div className='py-4 mt-4 text-center'>
+					<h2 className='text-3xl text-gray-700 tablet:text-4xl text-l bold mobile:text-4xl'>
 						Welcome to <span className='text-hive'>TidyHive!</span>
 					</h2>
-					<p className='text-2xl text-gray-600'>Sign in to access your account</p>
+					<p className='text-xl text-gray-600 tablet:text-2xl mobile:text-2xl'>Sign in to access your account</p>
 				</div>
-				<div className='w-4/5 max-w-lg'>
+				<div className='max-w-lg phone:w-4/5'>
 					{isLoading ? (
 						<Dimmer active inverted>
 							<Loader size='large'>Loading</Loader>
@@ -69,24 +69,26 @@ const SignInLanding = (props) => {
 								/>
 								{errors.email && <p>{errors.email.message}</p>}
 							</Form.Field>
-							<div>
+							<div className="flex flex-wrap">
 								<button
 									type='submit'
-									className='h-10 px-8 font-semibold tracking-wider text-white border rounded shadow-lg bg-hive hover:bg-orange-500'
+									className='w-full h-10 px-8 mt-2 font-semibold tracking-wider text-white border rounded shadow-lg tablet:w-1/3 bg-hive hover:bg-orange-500'
 								>
 									Submit
 								</button>
+                <div className="w-full mt-6 tablet:w-4/6 tablet:mt-2">
 								<button
-									className='h-10 px-8 ml-2 font-semibold text-gray-700 bg-gray-300 border rounded shadow-lg hover:bg-gray-400'
+									className='w-full h-10 px-4 font-semibold text-gray-700 bg-gray-300 border rounded shadow-lg hover:bg-gray-400 tablet:w-3/5 tablet:ml-2'
 									onClick={() => props.history.push('/forgot-password')}
 								>
 									Forgot Password
 								</button>
+                </div>
 							</div>
-							<div>
-								<p className='py-4 text-gray-700'>
+							<div className="mt-4">
+								<p className='py-4 text-sm gray-700 text-l phone:text-base'>
 									Don't have an account?{' '}
-									<Link to='/signup' className='font-semibold text-hive hover:text-orange-500'>
+									<Link to='/signup' className='text-sm font-semibold text-hive hover:text-orange-500 phone:text-base'>
 										Sign Up
 									</Link>
 								</p>
@@ -97,7 +99,7 @@ const SignInLanding = (props) => {
 							<div className='flex justify-center py-4'>
 								<button
 									onClick={googleAuth}
-									className='h-10 px-3 font-semibold tracking-wider text-white border rounded shadow-lg bg-hive hover:bg-orange-500'
+									className='w-full h-10 font-semibold tracking-wider text-white border rounded shadow-lg tablet:w-1/2 bg-hive hover:bg-orange-500'
 								>
 									<i className='ui icon google white' />
 									Sign in with Google
