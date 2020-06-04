69b02927594eeca29edaf64290bc891a4f246a8e
tauan-longaretti <tauanlongaretti@gmail.com>
2020-06-04 00:20:07 -0400

    new content added and tailwind styling being implemented

--- src/components/auth/Forgot-Password.js
+++ src/components/auth/Forgot-Password.js
@@ -1,17 +1,10 @@
 import React, { useState } from "react";
-import {
-  Form,
-  Button,
-  Message,
-  Header,
-  Icon,
-  Container,
-} from "semantic-ui-react";
+import { Form, Message, Container } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 
-import Navigation from "../layout/Navigation";
+import ForgotPassword from "../../assets/images/ForgotPassword.png";
 
 const ForgotPW = () => {
   const { register, handleSubmit, errors } = useForm();
@@ -31,13 +24,19 @@ const ForgotPW = () => {
 
   return (
     <>
-      <Container text>
-        <div align="center">
-          <Header as="h2" icon>
-            <Icon name="home" />
-            Welcome to TidyHive!
-            <Header.Subheader>Forgot Password</Header.Subheader>
-          </Header>
+      <div className="flex flex-col items-center justify-center h-full">
+        <div className="text-center">
+          <img
+            className="w-32 pb-12 m-auto tablet:w-48 desktop:w-64"
+            src={ForgotPassword}
+          />
+          <h1 className="text-gray-700 font-semi-bold">
+            Forgot Your Password?
+          </h1>
+          <h2>That's okay, it happens!</h2>
+          <h3>
+            Please enter your email below to proceed with reseting your email.
+          </h3>
         </div>
         <div>
           {/* {isLoading ? (
@@ -57,19 +56,26 @@ const ForgotPW = () => {
               {errors.username && <p>{errors.username.message}</p>}
             </Form.Field>
 
-            <Button type="submit">Submit</Button>
-            <Button size="small" type="back">
-              <Link to={"/"}>Back</Link>
-            </Button>
+            <button
+              type="submit"
+              className="w-full h-10 px-8 mt-4 font-semibold tracking-wider text-white border rounded shadow-lg tablet:w-2/5 bg-hive hover:bg-orange-500"
+            >
+              Submit
+            </button>
+            <button
+              type="back"
+              className="w-full h-10 px-4 mt-8 font-semibold text-gray-700 bg-gray-300 border rounded shadow-lg tablet:mt-4 hover:bg-gray-400 tablet:w-2/5 tablet:ml-6"
+            >
+              <Link to={"/signin"} className="hover:text-gray-800">Back</Link>
+            </button>
             <Message
               success={message}
               header="Please check your email!"
               content="If the email you've entered is associated with an account, you will be receiving an email to reset your password shortly."
             />
           </Form>
-          )}
         </div>
-      </Container>
+      </div>
     </>
   );
 };
--- src/components/auth/SignIn-Landing.js
+++ src/components/auth/SignIn-Landing.js
@@ -79,7 +79,7 @@ const SignInLanding = (props) => {
                   Submit
                 </button>
                 <button
-                  className="w-full h-10 px-4 mt-8 bg-gray-300 border rounded shadow-lg tablet:mt-4 font-semiboldtext-gray-700 hover:bg-gray-400 tablet:w-2/5 tablet:ml-6"
+                  className="w-full h-10 px-4 mt-8 font-semibold text-gray-700 bg-gray-300 border rounded shadow-lg tablet:mt-4 hover:bg-gray-400 tablet:w-2/5 tablet:ml-6"
                   onClick={() => props.history.push("/forgot-password")}
                 >
                   Forgot Password
