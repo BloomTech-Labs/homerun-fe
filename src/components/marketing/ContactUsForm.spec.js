3b909991a559053f11df2d128868f471d3f169ae
Batuhan Balta <bthn.balta@gmail.com>
2020-06-03 19:30:54 -0700

    commit before pull

---
+++ src/components/marketing/ContactUsForm.spec.js
@@ -0,0 +1,62 @@
+import ContactUsForm from "./ContactUsForm";
+import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
+afterAll(cleanup);
+
+describe("Render testing", () => {
+  it("Full Name form test", () => {
+    const { getByLabelText } = render(
+      <Router>
+        <ContactUsForm />
+      </Router>
+    );
+    const fullNameInput = getByLabelText(/Full Name*/i);
+    expect(fullNameInput).toBeVisible();
+  });
+  it("Email form test", () => {
+    const { getByLabelText } = render(
+      <Router>
+        <ContactUsForm />
+      </Router>
+    );
+    const emailInput = getByLabelText(/Email*/i);
+    expect(emailInput).toBeVisible();
+  });
+  it("Message form test", () => {
+    const { getByText } = render(
+      <Router>
+        <ContactUsForm />
+      </Router>
+    );
+    const messageInput = getByText(/Message*/i);
+    expect(messageInput).toBeVisible();
+  });
+});
+
+describe("functionality test", () => {
+  it("changing Full-Name values", () => {
+    const { getByLabelText, getByTestId } = render(
+      <Router>
+        <ContactUsForm />
+      </Router>
+    );
+    const fullNameInput = getByLabelText(/Full Name*/i);
+    fireEvent.change(fullNameInput, { target: { value: "test" } });
+    expect(fullNameInput.value).toBe("test");
+    fireEvent.click(getByTestId(/submit-test/i));
+  });
+  it("changing E-mail values", () => {
+    const { getByLabelText, getByTestId } = render(
+      <Router>
+        <ContactUsForm />
+      </Router>
+    );
+    const fullNameInput = getByLabelText(/Email*/i);
+    fireEvent.change(fullNameInput, { target: { value: "test-1" } });
+    expect(fullNameInput.value).toBe("test-1");
+    fireEvent.click(getByTestId(/submit-test/i));
+  });
+});
