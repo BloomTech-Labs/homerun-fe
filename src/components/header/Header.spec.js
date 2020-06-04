8c451f158e9419881dedb2251a0985ec0eaf489e
BenHall-7 <benjaminjahall@gmail.com>
2020-06-03 22:04:12 -0400

    Remove unused components before test is setup

---
+++ src/components/header/Header.spec.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+describe("Header", () => {
+  // test
+})
