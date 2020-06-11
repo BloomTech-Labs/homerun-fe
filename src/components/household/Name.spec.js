24410e83caa0b22910e97bbe74f76a05dc5b2f5f
BenHall-7 <benjaminjahall@gmail.com>
2020-06-10 23:59:25 -0400

    Add Name component tests

---
+++ src/components/household/Name.spec.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import Name from "./Name";
+
+// Source: https://github.com/ant-design/ant-design/issues/21096#issuecomment-578118486
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+});
+
+afterAll(cleanup);
+
+describe("Name component", () => {
+    it("Converts a name of 2 words into initials", () => {
+        let name = render(<Name name="Test name"/>);
+        let initials = name.getByTestId(/initials/);
+        expect(initials.textContent).toBe("TN");
+    });
+
+    it("Also contains the full name", () => {
+        let name = render(<Name name="Test name"/>);
+        let full = name.getByTestId(/full-name/);
+        expect(full.textContent).toBe("Test name");
+    })
+})
