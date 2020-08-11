f18110d8dcac79b60a21b1c2b2791622489a19a9
tauan-longaretti <tauanlongaretti@gmail.com>
2020-08-10 22:39:37 -0400

    added info for user to choose what perm level to assign to member they want to invite

--- src/components/household/InviteMember.js
+++ src/components/household/InviteMember.js
@@ -20,15 +20,42 @@ const InviteMember = (props) => {
     </Dimmer>
   ) : (
     <>
+    <section className="perm-info">
+          <h2 className="perm-title">Invite a new member</h2>
+          <p>Level 1 : May complete todos they are assigned to.</p>
+          <p>
+            Level 2 : May create todos and assign themselves + edit todos they
+            have created.
+          </p>
+          <p>
+            Level 3 : No restrictions on todos and may invite level 1 and 2 to
+            the group.
+          </p>
+        </section>
       <Form onSubmit={handleSubmit(onSubmit)}>
+        
         <Form.Field>
           <label>
-            Please Enter the email address of the user you wish to invite.
+            Please enter the email address of the user you wish to invite.
           </label>
           <input
             type="email"
             name="email"
             placeholder="user@email.com"
+            ref={register}
+          />
+          {stateError && <p className={'error'}>{stateError}</p>}
+        </Form.Field>
+        <Form.Field>
+          <label>
+            Please choose the permission level of the user you wish to invite.
+          </label>
+          <input
+            type="number"
+            name="level"
+            placeholder="1-3"
+            min="1"
+            max="3"
             ref={register}
           />
           {stateError && <p className={'error'}>{stateError}</p>}
