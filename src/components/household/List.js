5bdd0ff4f4333a10712672188a739e8ba5a294f8
tauan-longaretti <tauanlongaretti@gmail.com>
2020-08-15 00:56:54 -0400

    implemented new button for a modal that will allow user to edit permissions

--- src/components/household/List.js
+++ src/components/household/List.js
@@ -7,9 +7,11 @@ import actions from '../../actions/index';
 // Since this component itself is named List i had to import Semantic Ui's List component as UiList
 import { Button, Modal, List as UiList } from 'semantic-ui-react';
 import InviteMember from './InviteMember.js';
+import EditPermissions from './EditPermissions.js';
 
 const List = () => {
   const [memberModal, setMemberModal] = useState(false);
+  const [permissionsModal, setPermissionsModal] = useState(false);
 
   const household = useSelector((state) => state.household);
   const dispatch = useDispatch();
@@ -33,6 +35,14 @@ const List = () => {
           <Button onClick={() => setMemberModal(true)}>Invite Member</Button>
         }
         content={<InviteMember setModal={setMemberModal} />}
+      ></Modal>
+      <Modal
+        open={permissionsModal}
+        onClose={() => setPermissionsModal(false)}
+        trigger={
+          <Button onClick={() => setPermissionsModal(true)} className="edit-permissions">Edit Permissions</Button>
+        }
+        content={<EditPermissions setModal={setPermissionsModal} />}
       ></Modal>
     </div>
   );
