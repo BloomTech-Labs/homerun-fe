0ebebc2f86e77d3e39e4b39f0a411340bfbec5fa
Batuhan Balta <bthn.balta@gmail.com>
2020-08-20 16:54:19 -0700

    Invite member functionality test

--- src/components/household/InviteMember.spec.js
+++ src/components/household/InviteMember.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import InviteMember from './InviteMember';
@@ -29,7 +29,34 @@ describe('InviteMember rendering', () => {
         </Provider>
       </Router>
     );
-    const emailInput = getByTestId('permission-test');
-    expect(emailInput).toBeVisible();
+    const permissionInput = getByTestId('permission-test');
+    expect(permissionInput).toBeVisible();
+  });
+});
+
+describe('Invite Member functionality test', () => {
+  it('changing email values', () => {
+    const { getByTestId } = render(
+      <Router>
+        <Provider store={store}>
+          <InviteMember />
+        </Provider>
+      </Router>
+    );
+    const emailInput = getByTestId('email-test');
+    fireEvent.change(emailInput, { target: { value: 'test' } });
+    expect(emailInput.value).toBe('test');
+  });
+  it('changing permission level values', () => {
+    const { getByTestId } = render(
+      <Router>
+        <Provider store={store}>
+          <InviteMember />
+        </Provider>
+      </Router>
+    );
+    const permissionInput = getByTestId('permission-test');
+    fireEvent.change(permissionInput, { target: { value: '2' } });
+    expect(permissionInput.value).toBe('2');
   });
 });
